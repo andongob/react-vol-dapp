912f2bab7c20146cd80d1da15da67647dd525bf2
Ana <ndongo@gmail.com>
2024-08-30 18:15:08 +0200

    update secret

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com'
+      },
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com'
+      }
+    ]
+  }
+};
+
+module.exports = nextConfig;
